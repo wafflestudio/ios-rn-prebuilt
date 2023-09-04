5ced9ed4316d0e934c0227daa1c1ec9d6a3f8553
shinhong-park <shinhong.park@linecorp.com>
2023-09-05 01:23:42 +0900

    Add background parameter for RCTRootView

--- ReactNativeKit/ReactNativeKit/React.swift
+++ ReactNativeKit/ReactNativeKit/React.swift
@@ -7,12 +7,16 @@ import UIKit
 ///   - bundleURL: The URL to the JavaScript bundle file that should be loaded in the React Native application.
 ///   - moduleName: The name of the React Native module that should be run. This should correspond to a registered React Native module in your JavaScript bundle.
 ///   - initialProperties: The initial properties that should be passed to the React Native module. These are made available in the React Native application via this.props.
+///   - backgroundColor: The background color for the React Native View. Default is .clear.
 /// - Returns: A UIView configured to run the specified React Native application.
 public func makeReactView(
     bundleURL: URL,
     moduleName: String,
-    initialProperties: [AnyHashable: Any]?
+    initialProperties: [AnyHashable: Any]?,
+    backgroundColor: UIColor = .clear
 ) -> UIView
 {
-    return RCTRootView(bundleURL: bundleURL, moduleName: moduleName, initialProperties: initialProperties)
+    let rootView = RCTRootView(bundleURL: bundleURL, moduleName: moduleName, initialProperties: initialProperties)
+    rootView.backgroundColor = backgroundColor
+    return rootView
 }
