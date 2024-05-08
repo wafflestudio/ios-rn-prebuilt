3db3992898443d3e94b3fba0faa1dd831b937280
shinhong-park <shinhong.park@linecorp.com>
2024-05-09 02:07:42 +0900

    Use generic

--- ReactNativeKit/ReactNativeKit/EventEmitter/EventEmitter.swift
+++ ReactNativeKit/ReactNativeKit/EventEmitter/EventEmitter.swift
@@ -1,11 +1,11 @@
-public class EventEmitter {
+public class EventEmitter<EventType: SupportedEvent> {
     private let internalEmitter: RNEventEmitter
 
-    public init(supportedEvents: [any SupportedEvent]) {
+    public init(supportedEvents: [EventType]) {
         self.internalEmitter = RNEventEmitter(supportedEvents: supportedEvents)
     }
 
-    public func emitEvent(_ event: any SupportedEvent, payload: [AnyHashable: Any]? = [:]) {
+    public func emitEvent(_ event: EventType, payload: [AnyHashable: Any]? = [:]) {
         internalEmitter.sendEvent(withName: event.rawValue, body: payload)
     }
 }
