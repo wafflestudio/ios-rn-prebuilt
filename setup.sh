b823e9c69d600d224f9245bd002d3a81871b0a2c
shinhong-park <shinhong.park@linecorp.com>
2024-05-08 16:57:27 +0900

    setup 수정

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 ruby --version
+export NO_FLIPPER=1
+root=$(pwd)
 rm -rf ./react-native
 mkdir -p ./react-native
 cd ./react-native
-npx react-native init ReactNativeKit
+npx react-native init ReactNativeKit --version 0.72.3
 cd ./ReactNativeKit
 npm install @react-navigation/native react-native-screens react-native-safe-area-context @react-navigation/native-stack react-native-gesture-handler
 
-cd ../../../
+cd $root
 rm -rf ./react-native/ReactNativeKit/ios/*
 cp -r ./ReactNativeKit/* ./react-native/ReactNativeKit/ios/
+
 cd ./react-native/ReactNativeKit/ios
 pod install
 ./build.sh
+xed .
