49434159390ac62cd3992ef0fd124b5db795ed0d
shinhong-park <shinhong.park@linecorp.com>
2024-05-13 00:53:29 +0900

    Synchronize with js-side registration

--- ReactNativeKit/ReactNativeKit/EventEmitter/EventEmitter.swift
+++ ReactNativeKit/ReactNativeKit/EventEmitter/EventEmitter.swift
@@ -1,3 +1,5 @@
+import Combine
+
 public actor EventEmitter<EventType: SupportedEvent> {
     public init() {}
 
@@ -5,11 +7,35 @@ public actor EventEmitter<EventType: SupportedEvent> {
         RNEventEmitter.shared
     }
 
+    public var jsEventPublisher: AnyPublisher<JSEvent, Never> {
+        emitter.jsEventSubject.eraseToAnyPublisher()
+    }
+
     public func emitEvent(_ event: EventType, payload: [AnyHashable: Any]? = [:]) async {
+        if emitter.registerEventSubject.value != .register {
+            await waitForRegistration()
+        }
+        guard emitter.registerEventSubject.value == .register else {
+            assertionFailure("Event emitter is not registered even after waiting for registration.")
+            return
+        }
         if emitter.events.isEmpty {
             emitter.events = EventType.allEvents
         }
         emitter.sendEvent(withName: event.rawValue, body: payload)
+    }
+
+    private func waitForRegistration() async {
+        var cancellable: AnyCancellable?
+        await withCheckedContinuation { continuation in
+            cancellable = emitter.registerEventSubject
+                .sink { event in
+                    if event == .register {
+                        continuation.resume()
+                    }
+                }
+        }
+        cancellable?.cancel()
     }
 }
 
--- ReactNativeKit/ReactNativeKit/EventEmitter/RNEventEmitter.swift
+++ ReactNativeKit/ReactNativeKit/EventEmitter/RNEventEmitter.swift
@@ -1,8 +1,12 @@
 import Foundation
+import Combine
 @_implementationOnly import React
 
 @objc(RNEventEmitter)
 class RNEventEmitter: RCTEventEmitter {
+    let jsEventSubject = PassthroughSubject<JSEvent, Never>()
+    let registerEventSubject = CurrentValueSubject<RegisterEvent, Never>(.deregister)
+
     private static var _shared: RNEventEmitter?
     static var shared: RNEventEmitter {
         if let shared = _shared {
@@ -24,4 +28,22 @@ class RNEventEmitter: RCTEventEmitter {
     override func supportedEvents() -> [String] {
         events
     }
+
+    @objc func sendEventToNative(name: String, payload: [AnyHashable: Any]? = nil) {
+        if let registerEvent = RegisterEvent(rawValue: name) {
+            registerEventSubject.value = registerEvent
+        }
+        let jsEvent = JSEvent(name: name, payload: payload)
+        jsEventSubject.send(jsEvent)
+    }
+}
+
+public struct JSEvent {
+    let name: String
+    let payload: [AnyHashable: Any]?
+}
+
+enum RegisterEvent: String {
+    case register
+    case deregister
 }
